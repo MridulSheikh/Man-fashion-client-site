7156a014f06cfbec365a501218e6ea53300de613
MridulSheikh <prince9nazir@gmail.com>
2022-09-10 18:08:33 +0600

    inprove cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+const img = require("../assets/img/empty_cart.jpg")
 
 function Cart() {
   
@@ -16,30 +17,39 @@ function Cart() {
    }
 
   return (
-    <div className='container mx-auto grid grid-cols-2 gap-10'>
-      <div>
+    <div>
       {
-            item.map((i : any) => <div className='flex gap-4 mt-5 p-3 border rounded-md'>
-               <div className='w-20 h-20'>
-                <img src={i.img} alt="img" className='h-full' />
-               </div>
-               <div className="w-7/12">
-                <p className='font-sans font-semibold'>{i.name}</p>
-                <p className='font-normal text-2xl font-sans my-2'>${i.price}</p>
-                <p className='font-sans mb-2'>size : {i.size}</p>
-                <p className='font-sans'>quantity : {i.quantity}</p>
-                <button onClick={()=>handleDeletion(i.id)} className='btn-sm btn btn-error font-sans mt-4'>remove</button>
-               </div>
-            </div>)
-        }
-      </div>
-      <div>
-        <div className='border rounded-md mt-5 p-5 fixed top-20 right-52 w-60 '>
-           <p className='font-sans'>Total : {total}</p>
-           <p className='font-sans'>Items : {item.length}</p>
-           <button className='btn btn-sm font-sans mt-3'>Procced to shipping</button>
+        item.length == 0 ?
+        <div className='w-screen h-screen flex justify-center items-center'>
+          <img src={img} alt="img" className='h-52' />
+        </div>
+        :
+        <div className='container mx-auto grid grid-cols-2 gap-10'>
+        <div>
+        {
+              item.map((i : any) => <div className='flex gap-4 mt-5 p-3 border rounded-md'>
+                 <div className='w-20 h-20'>
+                  <img src={i.img} alt="img" className='h-full' />
+                 </div>
+                 <div className="w-7/12">
+                  <p className='font-sans font-semibold'>{i.name}</p>
+                  <p className='font-normal text-2xl font-sans my-2'>${i.price}</p>
+                  <p className='font-sans mb-2'>size : {i.size}</p>
+                  <p className='font-sans'>quantity : {i.quantity}</p>
+                  <button onClick={()=>handleDeletion(i.id)} className='btn-sm btn btn-error font-sans mt-4'>remove</button>
+                 </div>
+              </div>)
+          }
+        </div>
+        <div>
+          <div className='border rounded-md mt-5 p-5 fixed top-20 right-52 w-60 '>
+             <p className='font-sans'>Total : {total}</p>
+             <p className='font-sans'>Items : {item.length}</p>
+             <button className='btn btn-sm font-sans mt-3'>Procced to shipping</button>
+          </div>
         </div>
       </div>
+      }
     </div>
   )
 }
